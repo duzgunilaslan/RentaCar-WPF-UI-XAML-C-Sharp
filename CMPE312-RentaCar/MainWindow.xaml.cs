b3095e3273b15a8bbf579142785dc857e856aeb7
Duzgun ilaslan <ilaslanduzgun@gmail.com>
2021-01-06 15:12:02 +0300

    Final Stage

---
+++ CMPE312-RentaCar/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CMPE312_RentaCar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Grid_ToolTipOpening(object sender, ToolTipEventArgs e)
+        {
+            
+
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          /*  if (user.Text=="ali")
+            {
+                MessageBox.Show("sifre Dogru");
+            }
+            else
+                MessageBox.Show("sifre yanlıis"); */
+            if (password.Password.Equals("1234") && user.Text == "dask")
+            {
+                Window1 page2 = new Window1();
+                page2.Show();
+                this.Close();
+            }
+            else
+                MessageBox.Show("sifre yanlıis");
+        }
+    }
+}
---
+++ CMPE312-RentaCar/Window1.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Defaults;
+
+namespace CMPE312_RentaCar
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        static Random random;
+        public Window1()
+        {
+            InitializeComponent();
+            random = new Random();
+
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        private void ButtonChangePieChart_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SeriesCollection = new SeriesCollection
+                {
+                    new PieSeries
+                    {
+                        Title = "Rented Vehicle", Values= new ChartValues<ObservableValue> { new ObservableValue(random.Next(0, 50))},
+                        DataLabels=true
+                    },
+                    new PieSeries
+                    {
+                        Title = "Non-Rented Vehicle", Values= new ChartValues<ObservableValue> { new ObservableValue(random.Next(0, 50))},
+                        DataLabels=true
+                    },
+                    new PieSeries
+                    {
+                        Title = "Remanning Cars", Values= new ChartValues<ObservableValue> { new ObservableValue(random.Next(0, 50))},
+                        DataLabels=true
+                    },
+                };
+                DataContext = this;
+
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show("The PieChart Couldn't Upload !!! \n" + exp.Message);
+            }
+        }
+    }
+}
